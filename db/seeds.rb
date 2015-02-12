f67f0591126fa15e0a26de72ec2ba8faeec8b049
jeremie <marichal.jeremie@gmail.com>
2015-02-12 17:25:09 +0100

    appli ok no css for the moment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Ingredient.create(name: "Lemon")
+Ingredient.create(name: "Ice")
+Ingredient.create(name: "Ruhm")
+Ingredient.create(name: "Old Ruhm")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Mint leaves")
+Ingredient.create(name: "Calvados")
+Ingredient.create(name: "Cassis")
+Ingredient.create(name: "White Wine")
+Ingredient.create(name: "Beer")
+
